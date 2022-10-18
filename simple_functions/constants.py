c185bfae50845b8eae56f43737201469e7b4fd6b
acse-42bd98d8 <zz1622@ic.ac.uk>
2022-10-18 14:11:32 +0100

    solve_some_questions

--- simple_functions/constants.py
+++ simple_functions/constants.py
@@ -6,10 +6,11 @@ __all__ = ['pi']
 
 
 def pi(terms=1):
-    return 1./(2.*sqrt(2.)/9801.*rsum(terms))
+    return 1. / (2. * sqrt(2.) / 9801. * rsum(terms))
 
 
 @cache
 def rsum(n):
-    t = factorial(4*n)*(1103+26390*n)/(factorial(n)**4*396**(4*n))
-    return t + rsum(n-1) if n else t
+    t = factorial(
+        4 * n) * (1103 + 26390 * n) / (factorial(n)**4 * 396**(4 * n))
+    return t + rsum(n - 1) if n else t
